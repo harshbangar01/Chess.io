fee91500eb90eadca38349c5dc233e4857a2a246
harshbangar01 <harshbang098@gmail.com>
2025-07-15 22:50:54 +0530

    server

--- app.js
+++ app.js
@@ -64,6 +64,7 @@ io.on("connection", function (uniquesocket) {
     });
 });
 
-server.listen(3000, () => {
-    console.log("Server is running on port 3000");
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
